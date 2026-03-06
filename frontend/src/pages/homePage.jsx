a90eddd10fb79d1338dfc86be8eb657f71f132eb
selin <selinoktay2000@yahoo.com>
2026-03-06 08:30:20 +0300

    search by recipe frontend added

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react";
 import MultiSelect from "../components/MultiSelect";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import { getIngredients } from "../api/ingredientsApi";
 import { searchRecipes } from "../api/recipesApi"
 import { Autocomplete, TextField } from "@mui/material";
 
 function HomePage() {
   const [selectedIngredients, setSelectedIngredients] = useState([]);
-
+  const [searchMode, setSearchMode] = useState('');
   const [matchedRecipes, setMatchedRecipes] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+
+  const handleSearchModeSelection = (event) => {
+      setSearchMode(event.target.value);
+  };
+
   const handleSearch = async () => {
       const ingredient_id_list = selectedIngredients.map(ing => ing.id)
       const data = {
           "ingredients": ingredient_id_list,
           "search_mode": "strict"
-          }
+      }
     const res = await searchRecipes(data);
     setMatchedRecipes(res.data);
   };
 
-  const [ingredients, setIngredients] = useState([]);
   useEffect(() => {
          const loadIngredients = async () => {
              const res = await getIngredients();
@@ -46,13 +56,27 @@ function HomePage() {
         <TextField {...params} label="Ingredient" placeholder="ingredients" />
       )}
     />
-
-      <button
-        onClick={handleSearch}
-        style={{ marginTop: "1rem" }}
-      >
-        Search Recipes
-      </button>
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Search Mode</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={searchMode}
+          label="SearchMode"
+          onChange={handleSearchModeSelection}
+        >
+          <MenuItem value="strict">strict (no ingredients besides the ones selected)</MenuItem>
+          <MenuItem value="flexible">flexible (uses any of the selected ingredients but is not limited to them)</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+    <button
+      onClick={handleSearch}
+      style={{ marginTop: "1rem" }}
+    >
+      Search Recipes
+    </button>
     </div>
   );
 }
